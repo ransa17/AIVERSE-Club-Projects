651eb495e74cbb682f18eb4b5d92b3295459838d
ransa17 <148451537+ransa17@users.noreply.github.com>
2025-11-15 19:45:31 +0530

    Update and rename middleware.ts to proxy.ts

---
+++ proxy.ts
@@ -0,0 +1,11 @@
+export { auth as middleware } from "@/auth";
+
+import { auth } from "@/auth";
+
+export default auth;
+
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
